dee0cfe2556ce7610b05d191e4d076fb1bf52d79
jamshaidmehmood021 <jamshaid.mehmood@arbisoft.com>
2024-08-19 20:37:09 +0500

    updated admin.py to show the user table with relevent fields

--- django_backend/Users/admin.py
+++ django_backend/Users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from Users.models import CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email','country','skill')
