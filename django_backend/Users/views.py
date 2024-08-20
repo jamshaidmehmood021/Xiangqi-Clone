dc9a654018376f891f072caaeb617f82e6ec05c5
jamshaidmehmood021 <jamshaid.mehmood@arbisoft.com>
2024-08-20 15:06:25 +0500

    returning meaning full messages

--- django_backend/Users/views.py
+++ django_backend/Users/views.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import api_view
 from rest_framework import status  # Import status module
 from .serializers import UserRegisterSerializer
 from .serializers import MyTokenObtainPairView
+from django.contrib.auth.models import User
+from Users.models import CustomUser
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -13,11 +15,18 @@ def getRoutes(request):
     ]
 
     return Response(routes)
-
 @api_view(['POST'])
 def register_user(request):
+    if CustomUser.objects.filter(username=request.data.get('username')).exists():
+        return Response({'message': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if CustomUser.objects.filter(email=request.data.get('email')).exists():
+        return Response({'message': 'Email already exists'}, status=status.HTTP_400_BAD_REQUEST)
+    
     serializer = UserRegisterSerializer(data=request.data)
+    
     if serializer.is_valid():
         serializer.save()
         return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+    
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
