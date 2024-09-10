b2646a350f4feeaa3703c5f341797af49b986766
jamshaidmehmood021 <jamshaid.mehmood@arbisoft.com>
2024-09-10 18:07:33 +0500

    Updating axios.js for deployment

--- post-application/src/utils/axios/axios.js
+++ post-application/src/utils/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000',
+    //baseURL: 'http://localhost:5000',
+    baseURL: 'https://instagram-lite-backend.vercel.app',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
